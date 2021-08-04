dc8e536f507ee4432b71ced4509da35eb52d1c62
Lauren Dobratz <2316@c02zx0umjv3x.hbtn.io>
2021-08-04 17:26:12 -0500

    fixed trailing whitespaces

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -13,7 +13,7 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
    binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
-   
+
    if (newNode == NULL)
    {
       return NULL;
@@ -22,5 +22,5 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
    newNode->n = value;
    newNode->left = NULL;
    return (newNode);
-}
+   }
 
