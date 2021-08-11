2d7c4213e7ebdbda888e9f956270f64edd79b9d7
laurendobratz00 <61027706+laurendobratz00@users.noreply.github.com>
2021-08-11 16:58:31 -0500

    add docstrings

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 """
-A method that calculates the fewest number of operations needed to result in exactly n H chars in the file
+A method that calculates the fewest number of operations needed to 
+result in exactly n H chars in the file
 """
+
+
 def prime_factors(n):
+    """ helper function """
     i = 2
     factors = []
     while i * i <= n:
@@ -15,9 +19,11 @@ def prime_factors(n):
         factors.append(n)
     return factors
 
+
 def minOperations(n):
+    """ fewest number of operations """
     if n is int:
-        if n < 1:
+        if n < 2:
             return 0
         else:
             list = prime_factors(n)
