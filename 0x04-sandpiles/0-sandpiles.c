b4ac4363fa3033cd98d0cb12d7219a107dd1c817
laurendobratz00 <laurendobratz@icloud.com>
2021-09-28 22:09:15 +0000

    betty

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -11,15 +11,15 @@
  */
 void add(int grid1[3][3], int grid2[3][3])
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            grid1[i][j] = grid1[i][j] + grid2[i][j];
-        }
-    }
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			grid1[i][j] = grid1[i][j] + grid2[i][j];
+		}
+	}
 }
 
 /**
@@ -30,46 +30,46 @@ void add(int grid1[3][3], int grid2[3][3])
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    int i, j, z, newGrid[3][3];
+	int i, j, z, newGrid[3][3];
 
-    add(grid1, grid2);
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            newGrid[i][j] = grid1[i][j];
-        }
-    }
-    z = check_stable(grid1);
-    while (z == 1)
-    {
-        printf("=\n");
-        print_grid1(grid1);
-        for (i = 0; i < 3; i++)
-        {
-            for (j = 0; j < 3; j++)
-            {
-                if (grid1[i][j] >= 4)
-                    newGrid[i][j] -= 4;
-                if (i != 0 && grid1[i - 1][j] >= 4)
-                    newGrid[i][j]++;
-                if (i != 2 && grid1[i + 1][j] >= 4)
-                    newGrid[i][j]++;
-                if (j != 0 && grid1[i][j - 1] >= 4)
-                    newGrid[i][j]++;
-                if (j != 2 && grid1[i][j + 1] >= 4)
-                    newGrid[i][j]++;
-            }
-        }
-        for (i = 0; i < 3; i++)
-        {
-            for (j = 0; j < 3; j++)
-            {
-                grid1[i][j] = newGrid[i][j];
-            }
-        }
-        z = check_stable(grid1);
-    }
+	add(grid1, grid2);
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			newGrid[i][j] = grid1[i][j];
+		}
+	}
+	z = check_stable(grid1);
+	while (z == 1)
+	{
+		printf("=\n");
+		print_grid1(grid1);
+		for (i = 0; i < 3; i++)
+		{
+			for (j = 0; j < 3; j++)
+			{
+				if (grid1[i][j] >= 4)
+					newGrid[i][j] -= 4;
+				if (i != 0 && grid1[i - 1][j] >= 4)
+					newGrid[i][j]++;
+				if (i != 2 && grid1[i + 1][j] >= 4)
+					newGrid[i][j]++;
+				if (j != 0 && grid1[i][j - 1] >= 4)
+					newGrid[i][j]++;
+				if (j != 2 && grid1[i][j + 1] >= 4)
+					newGrid[i][j]++;
+			}
+		}
+		for (i = 0; i < 3; i++)
+		{
+			for (j = 0; j < 3; j++)
+			{
+				grid1[i][j] = newGrid[i][j];
+			}
+		}
+		z = check_stable(grid1);
+	}
 }
 
 /**
@@ -79,39 +79,39 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
  */
 int check_stable(int grid1[3][3])
 {
-    int i, j, num;
+	int i, j, num;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            num = grid1[i][j];
-            if (num >= 4)
-            {
-                return (1);
-            }
-        }
-    }
-    return (0);
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			num = grid1[i][j];
+			if (num >= 4)
+			{
+				return (1);
+			}
+		}
+	}
+	return (0);
 }
 
 /**
- * printgrid1 - function from main file that prints grid
+ * print_grid1 - function from main file that prints grid
  * @grid: sandpile
  * Return: void
  */
 void print_grid1(int grid[3][3])
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (j)
-                printf(" ");
-            printf("%d", grid[i][j]);
-        }
-        printf("\n");
-    }
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
 }
