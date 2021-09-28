e2bb483cd320c1e57b068563b371a007d5bfdc75
laurendobratz00 <laurendobratz@icloud.com>
2021-09-28 18:44:49 +0000

    add file

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" a method that determines if all the boxes can be opened """
+
+def canUnlockAll(boxes):
+    newlist = []
+    k = len(boxes)
+    for i in boxes:
+        if len(i) == 0 and i is not boxes[k-1]:
+            return False
+        for j in i:
+            newlist.append(j)
+    for index, keys in enumerate(boxes):
+        if index in newlist or index < k-1:
+            return True
+        else:
+            return False
