b0b897ba481af4b95ffe03aa7762e2f9436826b8
laurendobratz00 <laurendobratz@icloud.com>
2021-10-05 07:46:10 +0000

    exhausted all options

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -31,52 +31,57 @@ if __name__ == '__main__':
                 filesize = int(input.rsplit(' ', 1)[1])
             except IndexError:
                 pass
+            count += 1
             if (statuscode == 200):
                 count200 += 1
-                print("200:", count200)
             if (statuscode == 301):
                 count301 += 1
-                print("301:", count301)
             if (statuscode == 400):
                 count400 += 1
-                print("400:", count400)
             if (statuscode == 401):
                 count401 += 1
-                print("401:", count401)
             if (statuscode == 403):
                 count403 += 1
-                print("403:", count403)
             if (statuscode == 404):
                 count404 += 1
-                print("404:", count404)
             if (statuscode == 405):
                 count405 += 1
-                print("405:", count405)
             if (statuscode == 500):
                 count500 += 1
-                print("500:", count500)
-        if (count % 10 == 0):
-            print("Filesize:", filesize)
-        for i in range(0, (len(a))):
-            b = (a[i])
-            l = []
-            l = l.append(b)
-            if (statuscode == 200):
-                statuscodecount += 1
-            if (statuscode == 301):
-                statuscodecount += 1
-            if (statuscode == 400):
-                statuscodecount += 1
-            if (statuscode == 401):
-                statuscodecount += 1
-            if (statuscode == 403):
-                statuscodecount += 1
-            if (statuscode == 404):
-                statuscodecount += 1
-            if (statuscode == 405):
-                statuscodecount += 1
-            if (statuscode == 500):
-                statuscodecount += 1
-            count += 1
+            if (count % 10 == 0):
+                print("Filesize:", filesize)
+                if (count200 != 0):
+                    print("200:", count200)
+                if (count301 != 0):
+                    print("301:", count301)
+                if (count400 != 0):
+                    print("400:", count400)
+                if (count401 != 0):
+                    print("401:", count401)
+                if (count403 != 0):
+                    print("403:", count403)
+                if (count404 != 0):
+                    print("404:", count404)
+                if (count405 != 0):
+                    print("405:", count405)
+                if (count500 != 0):
+                    print("500:", count500)
+        print("File size:", filesize)
+        if (count200 != 0):
+            print("200:", count200)
+        if (count301 != 0):
+            print("301:", count301)
+        if (count400 != 0):
+            print("400:", count400)
+        if (count401 != 0):
+            print("401:", count401)
+        if (count403 != 0):
+            print("403:", count403)
+        if (count404 != 0):
+            print("404:", count404)
+        if (count405 != 0):
+            print("405:", count405)
+        if (count500 != 0):
+            print("500:", count500)
     except KeyboardInterrupt as err:
         pass
