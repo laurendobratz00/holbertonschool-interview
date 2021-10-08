92c2f324e77fb07804dd44decde937274a65ad9f
laurendobratz00 <laurendobratz@icloud.com>
2021-10-08 06:43:20 +0000

    i forgot betty :(

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -4,40 +4,40 @@
 /**
  * menger - a function that draws a 2D Menger Sponge
  * @level: the level of the Menger Sponge to draw
- * Return: 
+ * Return: void
  */
 
 void menger(int level)
 {
-    int width, height, z = pow(3, level);
-    int check;
-    char printchar;
+	int width, height, z = pow(3, level);
+	int check;
+	char printchar;
 
-    if (level < 0)
-    {
-        ;
-    }
-    if (level == 0)
-    {
-        printf("#\n");
-        return;
-    }
-    for (height = 0; height < z; height++)
-    {
-        for (width = 0; width < z; width++)
+	if (level < 0)
+	{
+		;
+	}
+	if (level == 0)
+	{
+		printf("#\n");
+		return;
+	}
+	for (height = 0; height < z; height++)
+	{
+		for (width = 0; width < z; width++)
 		{
-            check = 1;
-            printchar = '#';
+			check = 1;
+			printchar = '#';
 			for (; check < z; check *= 3)
 			{
-                if ((height / check) % 3 == 1 && (width / check) % 3 == 1)
-                {
-                    printchar = ' ';
-                    break;
-                }
-            }
-            printf("%c", printchar);
-        }
-        printf("\n");
-    }
+				if ((height / check) % 3 == 1 && (width / check) % 3 == 1)
+				{
+					printchar = ' ';
+					break;
+				}
+			}
+			printf("%c", printchar);
+		}
+		printf("\n");
+	}
 }
