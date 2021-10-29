38fdb670608d118abc14f260fbb3807efe0bd931
laurendobratz00 <laurendobratz@icloud.com>
2021-10-29 04:36:26 +0000

    add files

---
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * sorted_array_to_avl - a function that builds an AVL tree from an array
+ * @array: int pointer
+ * @size: size_t
+ * Return: a pointer to the root node of created AVL tree, or NULL on failure
+ */
+
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+
+}
