2011de0022f909875fd93e5c4dc41be9204d2489
laurendobratz00 <laurendobratz@icloud.com>
2021-11-12 03:40:08 +0000

    semistandard

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-let request = require('request');
+const request = require('request');
 const process = require('process');
 request.get('https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/', function (err, response, body) {
   if (err) {
