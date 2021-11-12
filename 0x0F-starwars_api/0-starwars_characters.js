0ec64072d3b294fb9ebb4a1073d2a11cf10d96b7
laurendobratz00 <laurendobratz@icloud.com>
2021-11-12 03:38:09 +0000

    add file

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+let request = require('request');
+const process = require('process');
+request.get('https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/', function (err, response, body) {
+  if (err) {
+    console.log(err);
+  }
+  const characters = JSON.parse(body).characters;
+  const dict = {};
+  const x = JSON.parse(body);
+  for (const c of x.characters) {
+    request(c, function (error, response, body) {
+      if (error) {
+        console.log(error);
+      }
+      const names = JSON.parse(body).name;
+      dict[c] = names;
+
+      if (Object.values(characters).length === characters.length) {
+        characters.forEach(c => {
+          console.log(dict[c]);
+        });
+      }
+    });
+  }
+});
