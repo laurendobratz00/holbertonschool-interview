dae05d19ff1266a02feb6e8431b75b3d64e356c4
laurendobratz00 <laurendobratz@icloud.com>
2021-11-28 21:35:15 +0000

    added new for loop

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -3,15 +3,32 @@
 
 def rain(walls):
     """ walls """
-    result = 0
+    """result = 0
     if not walls:
         return 0
     for i in walls:
-        maxLeft = 0
-        maxRight = 0
+        maxLeft = walls[i]
+        maxRight = walls[i]
         j = 0
         maxLeft = max(maxLeft, walls[j])
         for j in walls:
             maxRight = max(maxRight, walls[j])
         result += min(maxLeft, maxRight) - walls[i]
-    return result
+    return result"""
+
+    res = 0;
+    n = len(walls)
+
+    for i in range(1, n - 1) :
+         
+        left = walls[i];
+        for j in range(i) :
+            left = max(left, walls[j]);
+
+        right = walls[i];
+        for j in range(i + 1 , n) :
+            right = max(right, walls[j]);
+             
+        res = res + (min(left, right) - walls[i]);
+
+    return res;
