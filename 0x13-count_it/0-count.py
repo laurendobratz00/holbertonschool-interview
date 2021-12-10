3381c3f982521d4e3ba4ef210f508cda0d4ecccc
laurendobratz00 <laurendobratz@icloud.com>
2021-12-10 05:52:37 +0000

    change back

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -25,13 +25,10 @@ def count_words(subreddit, word_list, word_list_dict={}, count=0, after=None):
                      allow_redirects=False)
     data = x.json()
     r = data.get('data')
-    c = r.get('children')
     after = r.get('after')
-    if not x:
-        return None
-    # words = data.get('data').get('children')
+    words = data.get('data').get('children')
     # = Convert(word_list)
-    for s in c:
+    for s in words:
         body = (s.get('data')['title']).lower()
         for j in body.split():
             for key in word_list:
