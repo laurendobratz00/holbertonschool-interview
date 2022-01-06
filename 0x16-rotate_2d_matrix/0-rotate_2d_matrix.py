15eec574bd1f08de1b406f5f86586f683095ee0d
laurendobratz00 <laurendobratz@icloud.com>
2022-01-06 00:40:02 +0000

    pep8

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ rotate 2D matrix """
 
+
 def rotate_2d_matrix(matrix):
     """ function to rotate 2D matrix 90 degrees """
     N = len(matrix[0])
@@ -11,8 +12,8 @@ def rotate_2d_matrix(matrix):
             matrix[N - 1 - j][i] = matrix[N - 1 - i][N - 1 - j]
             matrix[N - 1 - i][N - 1 - j] = matrix[j][N - 1 - i]
             matrix[j][N - 1 - i] = temp
- 
-# Function to print the matrix
+
+
 def printMatrix(matrix):
     """ function to print the matrix """
     N = len(matrix[0])
