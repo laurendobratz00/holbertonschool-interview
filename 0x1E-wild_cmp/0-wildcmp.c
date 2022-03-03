36a52b70e5b61b1e1fe2af253faf1ea6ddc817b9
laurendobratz00 <laurendobratz@icloud.com>
2022-03-03 02:20:34 +0000

    took out loop

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -33,12 +33,11 @@ int compare(char a[], char b[])
 {
 	int flag = 0, i = 0;
 
-	while (a[i] != '\0' && b[i] != '\0')
+	if (a[i] != '\0' && b[i] != '\0')
 	{
 		if (a[i] != b[i])
 		{
 			flag = 1;
-			break;
 		}
 		i++;
 	}
