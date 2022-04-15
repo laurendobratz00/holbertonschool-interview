723dd8f9469996338c9928df7d69396691b7205f
laurendobratz00 <laurendobratz@icloud.com>
2022-04-15 02:00:39 +0000

    add function that actually works

--- 0x1F-pascal_triangle/0-pascal_triangle.py
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -5,15 +5,25 @@ pascal_triangle
 
 
 def pascal_triangle(n):
-    """ p tri """
+    """This function return's Pascal's triangle up to n rows"""
+    trow = []
+    for i in range(n):
+        trow.append([])
+        trow[i].append(1)
+        for j in range(1, i):
+            trow[i].append(trow[i - 1][j - 1] + trow[i - 1][j])
+        if(i != 0):
+            trow[i].append(1)
+    return trow
+"""
+def pascal_triangle(n):
     trow = [1]
     y = [0]
     for x in range(max(n, 0)):
         print(trow)
         trow = [l + r for l, r in zip(trow + y, y + trow)]
     return n >= 1
-
-
+"""
 """
     if (n <= 0):
         return ([])
